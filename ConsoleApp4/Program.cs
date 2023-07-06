a5a598a9bf37ea81be65bb4232ba838d83cd6fcc
abdullapro2006 <abdullahmanafli38@gmail.com>
2023-07-06 16:52:53 +0400

    Datetime created with timespan

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+      DateTime datetime = DateTime.ParseExact("06/07/2023" , "dd/MM/yyyy" , CultureInfo.InvariantCulture);
+        TimeSpan timespan = new TimeSpan(30, 6, 17);
+        var newdatetime = datetime.Add(timespan);
+        Console.WriteLine("Current datetime:" + datetime);
+        Console.WriteLine("1 month later datetime:" + newdatetime);
+
+
+
+
+
+
+        Console.Read();
+    }
+
+    
+}
